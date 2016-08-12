dff5608a3f6e8602adf9f1e803644bf15882fa13
seabornlee <seebornlee.cn@gmail.com>
2016-08-12 18:07:22 +0800

    write class in es6

--- views/home.js
+++ views/home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Home = React.createClass({
-    render: () => {
-        return <div>Home</div>;
-    }
-});
+class Home extends React.Component {
+  render() {
+     return <div>Hi</div>;
+  }
+}
 
 ReactDOM.render(<Home />, document.getElementById('content'));
