bd2c39f753f329645fe1b3d147f86bbba4de8863
seabornlee <seebornlee.cn@gmail.com>
2016-08-13 16:05:10 +0800

    add api to decrease coverage

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ function makeServer() {
     app.use(express.static('./public'));
     app.use(express.static('./views'));
 
+    app.get('/api/login', function (req, res) {
+        res.send("success");
+    });
+
     var server = app.listen(3000, function () {
         var port = server.address().port;
         console.log('Example app listening at port %s', port);
